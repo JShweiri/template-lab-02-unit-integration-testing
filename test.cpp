7ab3c4d62062d9fc1a50f6d836288b4c3552db93
JShweiri <wafflemanz3000@gmail.com>
2020-01-14 08:52:52 -0800

    added test cases

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 
 #include "gtest/gtest.h"
 
+#include <string>
+
 TEST(EchoTest, HelloWorld) {
     char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
@@ -11,6 +13,23 @@ TEST(EchoTest, EmptyString) {
     char* test_val[1]; test_val[0] = "./c-echo";
     EXPECT_EQ("", echo(1,test_val));
 }
+
+TEST(EchoTest, Nums) {
+   char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "1";
+    EXPECT_EQ("1", echo(2,test_val));
+}
+
+
+TEST(EchoTest, Symbol) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "$";
+    EXPECT_EQ("$", echo(2,test_val));
+}
+
+TEST(EchoTest, EmptySpace) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " ";
+    EXPECT_EQ(" ", echo(2,test_val));
+}
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
